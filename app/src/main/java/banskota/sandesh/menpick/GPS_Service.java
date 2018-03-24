fc8156b45124c265246294ba761985cdfd6dd143
sandeshworld <sb3vf@virginia.edu>
2018-03-24 19:12:35 -0400

    edit3

--- app/src/main/java/banskota/sandesh/menpick/GPS_Service.java
+++ app/src/main/java/banskota/sandesh/menpick/GPS_Service.java
@@ -1,24 +1,19 @@
 package banskota.sandesh.menpick;
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.app.Service;
 import android.content.Context;
-import android.content.pm.PackageManager;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
-import android.os.IBinder;
-import android.content.Intent;
-import android.support.v4.app.ActivityCompat;
 
 /**
- * Created by Sandesh Banskota on 3/24/2018.
+ * Created by filipp on 6/16/2016.
  */
-
 public class GPS_Service extends Service {
 
     private LocationListener listener;
@@ -30,50 +25,48 @@ public class GPS_Service extends Service {
         return null;
     }
 
-    @SuppressLint("MissingPermission")
     @Override
     public void onCreate() {
+
         listener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 Intent i = new Intent("location_update");
-                i.putExtra("coordinates", location.getLongitude()+" "+location.getLatitude());
+                i.putExtra("coordinates",location.getLongitude()+" "+location.getLatitude());
                 sendBroadcast(i);
             }
 
             @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
+            public void onStatusChanged(String s, int i, Bundle bundle) {
 
             }
 
             @Override
-            public void onProviderEnabled(String provider) {
+            public void onProviderEnabled(String s) {
 
             }
 
             @Override
-            public void onProviderDisabled(String provider) {
+            public void onProviderDisabled(String s) {
                 Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
             }
         };
+
         locationManager = (LocationManager) getApplicationContext().getSystemService(Context.LOCATION_SERVICE);
 
-
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 3000, 0, listener);
-
-
-
+        //noinspection MissingPermission
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,3000,0,listener);
 
     }
 
+    @Override
     public void onDestroy() {
         super.onDestroy();
-        if (locationManager != null) {
+        if(locationManager != null){
+            //noinspection MissingPermission
             locationManager.removeUpdates(listener);
         }
     }
-
-
 }
